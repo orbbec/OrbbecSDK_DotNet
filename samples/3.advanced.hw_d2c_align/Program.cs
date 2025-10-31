afe37af66e3b96ce6c1b59689b13f16bb91600ce
chuzihang <chuzihang@orbbec.com>
2025-10-31 15:17:58 +0800

    Refactor hw d2c align sample to .NET 8

--- samples/3.advanced.hw_d2c_align/Program.cs
+++ samples/3.advanced.hw_d2c_align/Program.cs
@@ -1,14 +1,218 @@
-using System;
+﻿using Orbbec;
+using Samples.Common;
 
-namespace Orbbec
+namespace Samples.HWD2CAlign
 {
     class Program
     {
-        [STAThread]
-        static void Main(string[] args)
+        private static volatile bool _isRunning = true;
+        private static volatile bool _enableAlignMode = true;
+        private static volatile float _alpha = 0.6f;
+
+        static void Main()
         {
-            var w = new HWD2CAlignWindow();
-            w.ShowDialog();
+            Console.Clear();
+            Console.WriteLine("HW D2C Align - Starting...");
+
+            Pipeline? pipe = null;
+            try
+            {
+                pipe = new Pipeline();
+                pipe.EnableFrameSync();
+
+                using var config = CreateHwD2CAlignConfig(pipe);
+                if (config == null)
+                {
+                    Console.WriteLine("Current device does not support hardware depth-to-color alignment.");
+                    return;
+                }
+
+                pipe.Start(config);
+
+                using (var renderer = new OrbbecRenderer(1280, 720, "HW D2C Align"))
+                {
+                    int d2cTextureIndex = renderer.AddVideoFrame();
+                    renderer.Closing += (e) =>
+                    {
+                        Console.WriteLine("Window closing, stopping...");
+                        _isRunning = false;
+                    };
+
+                    _ = Task.Run(() => HandleKeyPress(pipe, config));
+                    _ = Task.Run(() => StartStream(pipe, renderer, d2cTextureIndex));
+
+                    renderer.Run();
+                }
+            }
+            finally
+            {
+                pipe?.Stop();
+            }
+
+            Console.WriteLine("HW D2C Align sample exited.");
+        }
+
+        private static Config? CreateHwD2CAlignConfig(Pipeline pipe)
+        {
+            using var coloStreamProfiles = pipe.GetStreamProfileList(SensorType.OB_SENSOR_COLOR);
+            using var depthStreamProfiles = pipe.GetStreamProfileList(SensorType.OB_SENSOR_DEPTH);
+
+            // Iterate through all color and depth stream profiles to find a match for hardware depth-to-color alignment
+            uint colorSpCount = coloStreamProfiles.ProfileCount();
+            uint depthSpCount = depthStreamProfiles.ProfileCount();
+            for (int i = 0; i < colorSpCount; i++)
+            {
+                var colorProfile = coloStreamProfiles.GetProfile(i);
+                using var colorVsp = colorProfile.As<VideoStreamProfile>();
+
+                for (int j = 0; j < depthSpCount; j++)
+                {
+                    var depthProfile = depthStreamProfiles.GetProfile(j);
+                    using var depthVsp = depthProfile.As<VideoStreamProfile>();
+
+                    // make sure the color and depth stream have the same fps, due to some models may not support different fps
+                    if (colorVsp.GetFPS() != depthVsp.GetFPS() || colorVsp.GetFormat() == Format.OB_FORMAT_MJPG)
+                    {
+                        continue;
+                    }
+
+                    // Check if the given stream profiles support hardware depth-to-color alignment
+                    if (CheckIfSupportHWD2CAlign(pipe, colorProfile, depthProfile))
+                    {
+                        // If support, create a config for hardware depth-to-color alignment
+                        var hwD2CAlignConfig = new Config();
+                        hwD2CAlignConfig.EnableStream(colorProfile);                                                     // enable color stream
+                        hwD2CAlignConfig.EnableStream(depthProfile);                                                     // enable depth stream
+                        hwD2CAlignConfig.SetAlignMode(AlignMode.ALIGN_D2C_HW_MODE);                                      // enable hardware depth-to-color alignment
+                        hwD2CAlignConfig.SetFrameAggregateOutputMode(FrameAggregateOutputMode.OB_FRAME_AGGREGATE_OUTPUT_ALL_TYPE_FRAME_REQUIRE);  // output frameset with all types of frames
+                        return hwD2CAlignConfig;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static bool CheckIfSupportHWD2CAlign(Pipeline pipeline, StreamProfile colorStreamProfile, StreamProfile depthStreamProfile)
+        {
+            using var hwD2CSupportedDepthStreamProfiles = pipeline.GetD2CDepthProfileList(colorStreamProfile, AlignMode.ALIGN_D2C_HW_MODE);
+            if (hwD2CSupportedDepthStreamProfiles.ProfileCount() == 0)
+                return false;
+
+            // Iterate through the supported depth stream profiles and check if there is a match with the given depth stream profile
+            var depthVsp = depthStreamProfile.As<VideoStreamProfile>();
+            int count = (int)hwD2CSupportedDepthStreamProfiles.ProfileCount();
+            for (int i = 0; i < count; i++)
+            {
+                var sp = hwD2CSupportedDepthStreamProfiles.GetProfile(i);
+                var vsp = sp.As<VideoStreamProfile>();
+                if (vsp.GetWidth() == depthVsp.GetWidth() && vsp.GetHeight() == depthVsp.GetHeight() && vsp.GetFormat() == depthVsp.GetFormat()
+                    && vsp.GetFPS() == depthVsp.GetFPS())
+                {
+                    // Found a matching depth stream profile, it is means the given stream profiles support hardware depth-to-color alignment
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static async Task HandleKeyPress(Pipeline pipeline, Config config)
+        {
+            Console.WriteLine("'T': Enable/Disable HwD2C, '+/-': Adjust Transparency");
+            while (_isRunning)
+            {
+                if (!Console.KeyAvailable)
+                {
+                    await Task.Delay(50);
+                    continue;
+                }
+
+                var keyInfo = Console.ReadKey(true);
+                if (keyInfo.Key == ConsoleKey.T)
+                {
+                    _enableAlignMode = !_enableAlignMode;
+
+                    try
+                    {
+                        if (_enableAlignMode)
+                        {
+                            config.SetAlignMode(AlignMode.ALIGN_D2C_HW_MODE);
+                            Console.WriteLine("Haeware Depth to Color Align: Enabled");
+                        }
+                        else
+                        {
+                            config.SetAlignMode(AlignMode.ALIGN_DISABLE);
+                            Console.WriteLine("Haeware Depth to Color Align: Disabled");
+                        }
+
+                        pipeline.Stop();
+                        pipeline.Start(config);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Unexpected error: {ex.Message}");
+                    }
+                }
+                else if (keyInfo.Key == ConsoleKey.Add || keyInfo.Key == ConsoleKey.OemPlus)
+                {
+                    _alpha = Math.Min(_alpha + 0.1f, 1.0f);
+                    Console.WriteLine($"Adjust alpha to {_alpha:F2}");
+                }
+                else if (keyInfo.Key == ConsoleKey.Subtract || keyInfo.Key == ConsoleKey.OemMinus)
+                {
+                    _alpha = Math.Max(_alpha - 0.1f, 0.0f);
+                    Console.WriteLine($"Adjust alpha to {_alpha:F2}");
+                }
+            }
+        }
+
+        private static void StartStream(Pipeline pipeline, OrbbecRenderer renderer, int d2cTextureIndex)
+        {
+            try
+            {
+                while (_isRunning)
+                {
+                    using var frameSet = pipeline.WaitForFrames(100);
+                    if (frameSet == null) continue;
+
+                    using var colorFrame = frameSet?.GetColorFrame();
+                    using var depthFrame = frameSet?.GetDepthFrame();
+
+                    if (colorFrame == null || depthFrame == null)
+                        continue;
+
+                    byte[] d2cData = new byte[colorFrame.GetDataSize()];
+                    DepthOverlayColorProcess(colorFrame, depthFrame, ref d2cData);
+                    renderer.UpdateVideoFrame(d2cTextureIndex, (int)colorFrame.GetWidth(),
+                        (int)colorFrame.GetHeight(), Format.OB_FORMAT_RGB, d2cData);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Start stream error: {ex.Message}");
+            }
+        }
+
+        private static void DepthOverlayColorProcess(VideoFrame colorFrame, VideoFrame depthFrame, ref byte[] d2cData)
+        {
+            try
+            {
+                int colorW = (int)colorFrame.GetWidth();
+                int colorH = (int)colorFrame.GetHeight();
+                byte[] colorData = new byte[colorFrame.GetDataSize()];
+                colorFrame.CopyData(ref colorData);
+
+                int depthW = (int)depthFrame.GetWidth();
+                int depthH = (int)depthFrame.GetHeight();
+                byte[] depthDataRaw = new byte[depthFrame.GetDataSize()];
+                depthFrame.CopyData(ref depthDataRaw);
+                byte[] depthData = ImageConverter.ConvertY16ToRgb(depthW, depthH, depthDataRaw);
+
+                d2cData = ImageConverter.DepthAlignToColor(colorW, colorH, colorData, depthW, depthH, depthData, _alpha);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Start stream error: {ex.Message}");
+            }
         }
     }
 }
--- samples/common/ImageConverter.cs
+++ samples/common/ImageConverter.cs
@@ -12,13 +12,13 @@ namespace Samples.Common
         public static byte[] ConvertY8ToRgb(int width, int height, ReadOnlySpan<byte> y8Data)
         {
             if (y8Data == null)
-                throw new ArgumentException(nameof(y8Data));
+                throw new ArgumentException("Y8 data is null");
 
             int pixelCount = width * height;
             int rgbSize = pixelCount * 3;
 
             if (y8Data.Length < pixelCount)
-                throw new ArgumentException("Invalid Y8 buffer length");
+                throw new ArgumentException("Invalid Y8 data length");
 
             byte[] rgbData = new byte[rgbSize];
 
@@ -36,13 +36,13 @@ namespace Samples.Common
         public static byte[] ConvertY16ToRgb(int width, int height, ReadOnlySpan<byte> y16Data, bool normalize = true)
         {
             if (y16Data == null)
-                throw new ArgumentException(nameof(y16Data));
+                throw new ArgumentException("Y16 data is null");
 
             int pixelCount = width * height;
             int rgbSize = pixelCount * 3;
 
             if (y16Data.Length < pixelCount * 2)
-                throw new ArgumentException("Invalid Y16 buffer length");
+                throw new ArgumentException("Invalid Y16 data length");
 
             byte[] rgbData = new byte[rgbSize];
 
@@ -69,6 +69,58 @@ namespace Samples.Common
             }
 
             return rgbData;
+        }
+
+        public static byte[] DepthAlignToColor(int colorW, int colorH, ReadOnlySpan<byte> colorData,
+            int depthW, int depthH, ReadOnlySpan<byte> depthData, float alpha)
+        {
+            if (colorData == null || depthData == null)
+                throw new ArgumentException("Data is null");
+
+            if (alpha <= 0)
+                return colorData.ToArray();
+            else if (alpha > 1)
+                return depthData.ToArray();
+
+            float scaleW = (float)colorW / depthW;
+            float scaleH = (float)colorH / depthH;
+
+            int colorDataSize = colorW * colorH * 3;
+            int depthDataSize = depthW * depthH * 3;
+
+            byte[] output = new byte[colorData.Length];
+            colorData.CopyTo(output);
+
+            int colorIndex, depthIndex;
+            int srcX, srcY;
+
+            for (int j = 0; j < colorH; ++j)
+            {
+                for (int i = 0; i < colorW; ++i)
+                {
+                    srcX = (int)(i / scaleW);
+                    srcY = (int)(j / scaleH);
+
+                    colorIndex = (j * colorW + i) * 3;
+                    depthIndex = (srcY * depthW + srcX) * 3;
+
+                    if (depthIndex + 3 > depthDataSize || colorIndex + 3 > colorDataSize)
+                        continue;
+
+                    byte d0 = depthData[depthIndex];
+                    byte d1 = depthData[depthIndex + 1];
+                    byte d2 = depthData[depthIndex + 2];
+
+                    if (d0 == 0 && d1 == 0 && d2 == 0)
+                        continue;
+
+                    output[colorIndex] = (byte)(output[colorIndex] * (1.0f - alpha) + d0 * alpha);
+                    output[colorIndex + 1] = (byte)(output[colorIndex + 1] * (1.0f - alpha) + d1 * alpha);
+                    output[colorIndex + 2] = (byte)(output[colorIndex + 2] * (1.0f - alpha) + d2 * alpha);
+                }
+            }
+
+            return output;
         }
 
         // public static byte[] ConvertDepthToRGBData(byte[] depthData)
