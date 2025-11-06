d1efc8cd1db793b7dab3aa062f19f3eecbfa9dde
chuzihang <chuzihang@orbbec.com>
2025-11-06 10:52:42 +0800

    Improve record sample code structure and add exit handling

--- samples/2.device.record/Program.cs
+++ samples/2.device.record/Program.cs
@@ -9,10 +9,18 @@ namespace Samples.Record
         private static volatile bool _isRunning = true;
         private static volatile bool _isPaused = false;
 
-        static void Main()
+        static void Main(string[] args)
         {
             Console.Clear();
             Console.WriteLine("Record - Starting...");
+
+            using var renderer = new OrbbecRenderer(title: "Record");
+
+            Console.CancelKeyPress += (s, e) =>
+            {
+                e.Cancel = true;
+                renderer.Close();
+            };
 
             Context? ctx = null;
             Pipeline? pipe = null;
@@ -35,53 +43,65 @@ namespace Samples.Record
                     return;
                 }
 
-                device = deviceList.GetDevice(0);
-                pipe = new Pipeline(device);
-                var imuPipe = new Pipeline(device);
-
                 ctx.EnableDeviceClockSync(0);
 
+                device = deviceList.GetDevice(0);
+                pipe = new Pipeline(device);
+                using var config = new Config();
                 using var recordDevice = new RecordDevice(device, filePath);
 
-                using (var renderer = new OrbbecRenderer(1280, 720, "Record"))
+                Dictionary<SensorType, int> textureIndices = [];
+                using var sensorList = device.GetSensorList();
+                for (uint i = 0; i < sensorList.SensorCount(); ++i)
                 {
-                    Dictionary<SensorType, int> textureIndices = [];
-                    using var sensorList = device.GetSensorList();
-                    for (uint i = 0; i < sensorList.SensorCount(); ++i)
+                    var sensorType = sensorList.SensorType(i);
+                    if (sensorType == SensorType.OB_SENSOR_ACCEL || sensorType == SensorType.OB_SENSOR_GYRO)
                     {
-                        var sensorType = sensorList.SensorType(i);
-                        if (sensorType == SensorType.OB_SENSOR_ACCEL || sensorType == SensorType.OB_SENSOR_GYRO)
-                            continue;
-
-                        textureIndices.Add(sensorType, renderer.AddVideoFrame());
+                        config.EnableStream(sensorType);
+                        continue;
                     }
-                    renderer.Closing += (e) =>
+
+                    var format = sensorType switch
                     {
-                        Console.WriteLine("Window closing, stopping...");
-                        if (_isPaused)
-                        {
-                            recordDevice.Resume();
-                            _isPaused = false;
-                        }
-                        _isRunning = false;
+                        SensorType.OB_SENSOR_COLOR => Format.OB_FORMAT_RGB,
+                        SensorType.OB_SENSOR_DEPTH => Format.OB_FORMAT_Y16,
+                        SensorType.OB_SENSOR_IR or SensorType.OB_SENSOR_IR_LEFT
+                            or SensorType.OB_SENSOR_IR_RIGHT => Format.OB_FORMAT_Y8,
+                        _ => Format.OB_FORMAT_UNKNOWN
                     };
-
-                    _ = Task.Run(async () => await HandleKeyPress(recordDevice));
-                    _ = Task.Run(() => StartStream(pipe, renderer, textureIndices));
-                    _ = Task.Run(() => StartIMU(imuPipe));
-
-                    renderer.Run();
+                    config.EnableVideoStream(sensorType, 0, 0, 0, format);
+                    textureIndices.Add(sensorType, renderer.AddVideoFrame());
                 }
+                pipe.Start(config);
+
+                renderer.Closing += (e) =>
+                {
+                    Console.WriteLine("Window closing, stopping...");
+                    if (_isPaused)
+                    {
+                        recordDevice.Resume();
+                        _isPaused = false;
+                    }
+                    _isRunning = false;
+                };
+
+                _ = Task.Run(async () => await HandleKeyPress(recordDevice));
+                _ = Task.Run(() => StartStream(pipe, renderer, textureIndices));
+
+                renderer.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
             }
             finally
             {
                 pipe?.Stop();
-                pipe?.Dispose();
                 device?.Dispose();
                 ctx?.Dispose();
+                Console.WriteLine("Record sample exited.");
+                Environment.Exit(0);
             }
-
-            Console.WriteLine("Record sample exited.");
         }
 
         private static async Task HandleKeyPress(RecordDevice recorder)
@@ -118,28 +138,52 @@ namespace Samples.Record
         {
             try
             {
-                using var config = new Config();
-
-                foreach (var sensorType in textureIndices.Keys)
-                {
-                    var format = sensorType switch
-                    {
-                        SensorType.OB_SENSOR_COLOR => Format.OB_FORMAT_RGB,
-                        SensorType.OB_SENSOR_DEPTH => Format.OB_FORMAT_Y16,
-                        SensorType.OB_SENSOR_IR or SensorType.OB_SENSOR_IR_LEFT
-                            or SensorType.OB_SENSOR_IR_RIGHT => Format.OB_FORMAT_Y8,
-                        _ => Format.OB_FORMAT_UNKNOWN
-                    };
-                    config.EnableVideoStream(sensorType, 0, 0, 0, format);
-                    Console.WriteLine($"Enabled stream for: {sensorType}");
-                }
-
-                pipeline.Start(config);
-
                 while (_isRunning)
                 {
                     using var frameSet = pipeline.WaitForFrames(100);
                     if (frameSet == null) continue;
+
+                    using var accelFrameRaw = frameSet.GetFrame(FrameType.OB_FRAME_ACCEL);
+                    if (accelFrameRaw != null)
+                    {
+                        using var accelFrame = accelFrameRaw.As<AccelFrame>();
+                        var accelIndex = accelFrame.GetIndex();
+                        var accelTimeStampUs = accelFrame.GetTimeStampUs();
+                        var accelTemperature = accelFrame.GetTemperature();
+                        var accelType = accelFrame.GetFrameType();
+                        if (accelIndex % 50 == 0)
+                        {
+                            // print information every  50 frames.
+                            // var accelValue = accelFrame.GetAccelValue();
+                            // var obFloat3d = new Float3D { x = accelValue.x, y = accelValue.y, z = accelValue.z };
+                            var dataPtr = accelFrame.GetDataPtr();
+                            if (dataPtr == IntPtr.Zero)
+                                continue;
+                            var obFloat3d = Marshal.PtrToStructure<Float3D>(dataPtr);
+                            PrintImuValue(obFloat3d, accelIndex, accelTimeStampUs, accelTemperature, accelType, "m/s^2");
+                        }
+                    }
+
+                    using var gyroFrameRaw = frameSet.GetFrame(FrameType.OB_FRAME_GYRO);
+                    if (gyroFrameRaw != null)
+                    {
+                        using var gyroFrame = gyroFrameRaw.As<GyroFrame>();
+                        var gyroIndex = gyroFrame.GetIndex();
+                        var gyroTimeStampUs = gyroFrame.GetTimeStampUs();
+                        var gyroTemperature = gyroFrame.GetTemperature();
+                        var gyroType = gyroFrame.GetFrameType();
+                        if (gyroIndex % 50 == 0)
+                        {
+                            // print information every 50 frames.
+                            // var gyroValue = gyroFrame.GetGyroValue();
+                            // var obFloat3d = new Float3D { x = gyroValue.x, y = gyroValue.y, z = gyroValue.z };
+                            var dataPtr = gyroFrame.GetDataPtr();
+                            if (dataPtr == IntPtr.Zero)
+                                continue;
+                            var obFloat3d = Marshal.PtrToStructure<Float3D>(dataPtr);
+                            PrintImuValue(obFloat3d, gyroIndex, gyroTimeStampUs, gyroTemperature, gyroType, "rad/s");
+                        }
+                    }
 
                     foreach (var (sensorType, textureIndex) in textureIndices)
                     {
@@ -153,16 +197,14 @@ namespace Samples.Record
                             _ => FrameType.OB_FRAME_UNKNOWN
                         };
 
-                        var frame = frameSet.GetFrame(frameType);
+                        using var frame = frameSet.GetFrame(frameType);
 
                         if (frame != null)
                         {
-                            var vf = frame.As<VideoFrame>();
+                            using var vf = frame.As<VideoFrame>();
                             byte[] data = new byte[vf.GetDataSize()];
                             vf.CopyData(ref data);
                             renderer.UpdateVideoFrame(textureIndex, (int)vf.GetWidth(), (int)vf.GetHeight(), vf.GetFormat(), data);
-                            vf.Dispose();
-                            frame.Dispose();
                         }
                     }
                 }
@@ -170,63 +212,6 @@ namespace Samples.Record
             catch (Exception ex)
             {
                 Console.WriteLine($"Start stream error: {ex.Message}");
-            }
-        }
-
-        private static void StartIMU(Pipeline pipeline)
-        {
-            try
-            {
-                using var config = new Config();
-                config.EnableAccelStream();
-                config.EnableGyroStream();
-                pipeline.Start(config);
-
-                while (_isRunning)
-                {
-                    using var frameSet = pipeline.WaitForFrames(100);
-                    if (frameSet == null) continue;
-
-                    using var accelFrameRaw = frameSet.GetFrame(FrameType.OB_FRAME_ACCEL);
-                    using var accelFrame = accelFrameRaw.As<AccelFrame>();
-                    var accelIndex = accelFrame.GetIndex();
-                    var accelTimeStampUs = accelFrame.GetTimeStampUs();
-                    var accelTemperature = accelFrame.GetTemperature();
-                    var accelType = accelFrame.GetFrameType();
-                    if (accelIndex % 50 == 0)
-                    {
-                        // print information every  50 frames.
-                        // var accelValue = accelFrame.GetAccelValue();
-                        // var obFloat3d = new Float3D { x = accelValue.x, y = accelValue.y, z = accelValue.z };
-                        var dataPtr = accelFrame.GetDataPtr();
-                        if (dataPtr == IntPtr.Zero)
-                            continue;
-                        var obFloat3d = Marshal.PtrToStructure<Float3D>(dataPtr);
-                        PrintImuValue(obFloat3d, accelIndex, accelTimeStampUs, accelTemperature, accelType, "m/s^2");
-                    }
-
-                    using var gyroFrameRaw = frameSet.GetFrame(FrameType.OB_FRAME_GYRO);
-                    using var gyroFrame = gyroFrameRaw.As<GyroFrame>();
-                    var gyroIndex = gyroFrame.GetIndex();
-                    var gyroTimeStampUs = gyroFrame.GetTimeStampUs();
-                    var gyroTemperature = gyroFrame.GetTemperature();
-                    var gyroType = gyroFrame.GetFrameType();
-                    if (gyroIndex % 50 == 0)
-                    {
-                        // print information every 50 frames.
-                        // var gyroValue = gyroFrame.GetGyroValue();
-                        // var obFloat3d = new Float3D { x = gyroValue.x, y = gyroValue.y, z = gyroValue.z };
-                        var dataPtr = gyroFrame.GetDataPtr();
-                        if (dataPtr == IntPtr.Zero)
-                            continue;
-                        var obFloat3d = Marshal.PtrToStructure<Float3D>(dataPtr);
-                        PrintImuValue(obFloat3d, gyroIndex, gyroTimeStampUs, gyroTemperature, gyroType, "rad/s");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Start IMU error: {ex.Message}");
             }
         }
 
